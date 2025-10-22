b282bb6b3d0904f922f2613f0b90d5cf097a7d2e
Ben Northridge <bennorthridge@hotmail.com>
2025-10-22 20:53:18 +0100

    Quick bug fix, removed submodule from template to convert to normal folder

---
+++ schemalab-ui/src/router.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/"></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
