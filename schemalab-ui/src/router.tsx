34f1a397230fa2bb426ddc9680813cf0e1a49a30
Shriharisha, Sanjay (UG - Comp Sci & Elec Eng) <ss04662@surrey.ac.uk>
2025-11-02 21:18:18 +0000

    Created basic reactflow canvas and a non-displaying toolbox.

--- schemalab-ui/src/router.tsx
+++ schemalab-ui/src/router.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { DbDesigner } from "./pages/db-designer/db-designer";
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/dev/db-designer" element={<DbDesigner example="" />} />
                 <Route path="/"></Route>
             </Routes>
         </BrowserRouter>
