4428ad0028f225c76ac32ac9527beea76cbdcf40
Jonathan <jp01821@surrey.ac.uk>
2025-11-05 15:39:04 +0000

    deleteFiles function created

--- schema/functions/file.js
+++ schema/functions/file.js
@@ -32,18 +32,48 @@ function generateFiles(sqlString, projectName) {
         }
     });
     newdb.close();
-
+    
+    try {
     // create sql file
     fs.writeFileSync(`${filePath}${fileName}.sql`, sqlString);
 
     // write to info file
     fs.writeFileSync(`${filePath}fileInfo.json`, JSON.stringify(info));
+    } catch (err) {
+        console.log("Error writing to file: " + err);
+        throw "F00";
+    }
 
     return fileName;
 }
 
 function deleteFiles() {
     // delete all files past the delete time specified in fileInfo
+    var info = JSON.parse(fs.readFileSync(`${filePath}fileInfo.json`, 'utf-8'));
+    var keep = [];
+    const date = Date.now();
+    for (var item of info.deleteTimes) {
+        if (item.deleteTime < date) {
+            // delete files
+            try {
+                fs.unlinkSync(`${filePath}${item.fileName}.db`)
+                fs.unlinkSync(`${filePath}${item.fileName}.sql`)
+            } catch (err) {
+                console.log("Error deleting temp files: " + err);
+                throw "F00";
+            }
+        } else {
+            // add the item to the new array to be kept in fileInfo
+            keep.push(item)
+        }
+    }
+    info.deleteTimes = keep;
+    try {
+        fs.writeFileSync(`${filePath}fileInfo.json`, JSON.stringify(info));
+    } catch (err) {
+        console.log("Error writing to file: " + err);
+        throw "F00";
+    }
 }
 
 module.exports = {generateFiles, deleteFiles};
