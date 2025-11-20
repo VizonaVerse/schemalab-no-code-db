5960d201aa016a63888296775a409dfa93edeba6
xinru <xw00892@surrey.ac.uk>
2025-11-20 11:29:26 +0100

    Add password reset functionality with request and confirmation views

--- auth/accounts/urls.py
+++ auth/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, ProfileView, LogoutView, MyTokenObtainPairView
+from .views import RegisterView, ProfileView, LogoutView, MyTokenObtainPairView, PasswordResetRequestView, PasswordResetConfirmView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', ProfileView.as_view(), name='profile'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('password-reset/', PasswordResetRequestView.as_view(), name='password_reset_request'),
+    path('password-reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
