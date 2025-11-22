d659d3a83d53ea53a4c16f030ca3774368c1cfbf
xinru <xw00892@surrey.ac.uk>
2025-11-22 14:27:43 +0100

    Add email functionality for password reset and configure email settings

--- auth/auth_project/settings.py
+++ auth/auth_project/settings.py
@@ -153,3 +153,10 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 AUTHENTICATION_BACKENDS = ['accounts.backends.EmailBackend']
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')         
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_SSL = False                               
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')      
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD') 
