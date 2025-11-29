e81ac4b06aa97ff48ee80c451ab89ad213eafe6e
Sanjay Shriharisha <sanjay.sh@live.com>
2025-11-29 12:27:57 +0000

    Implemented basic display of projects page.

--- schemalab-ui/src/router.tsx
+++ schemalab-ui/src/router.tsx
@@ -1,13 +1,18 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/auth-context";
 import { DbDesigner } from "./pages/db-designer/db-designer";
+import { ProjectManagement } from "./pages/project-management/project-management";
 
 export default function Router() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/dev/db-designer" element={<DbDesigner example="" />} />
-                <Route path="/"></Route>
-            </Routes>
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/projects" element={<ProjectManagement />} />
+                    <Route path="/dev/db-designer" element={<DbDesigner example="" />} />
+                    <Route path="/" />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
     );
 }
