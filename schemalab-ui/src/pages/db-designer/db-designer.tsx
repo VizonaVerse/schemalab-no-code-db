16a9f7f551a3d3cde175aa2f4db31a87238bf46e
Sanjay Shriharisha <sanjay.sh@live.com>
2025-12-02 18:12:03 +0000

    New project correctly clears canvas and put requests work successfully. Loading of existing projects is currently being implemented.

--- schemalab-ui/src/pages/db-designer/db-designer.tsx
+++ schemalab-ui/src/pages/db-designer/db-designer.tsx
@@ -1,32 +1,62 @@
-import { useState } from "react";
 import { Canvas } from "./canvas/canvas";
 import { Topbar } from "./topbar/topbar";
-import { TableNode } from "./elements/tableNode/table-node";
-import { OneToOneEdge } from "./elements/relationships/O2O";
-import { OneToManyEdge } from "./elements/relationships/O2M";
-import { ManyToManyEdge } from "./elements/relationships/M2M";
-import "./db-designer.scss";
 import { ReactFlowProvider } from "reactflow";
-import tableIcon from "../../assets/toolbox/table.svg";
-import { CanvasProvider } from "../../contexts/canvas-context";
+import "./db-designer.scss";
+import { useLocation, useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useCanvasContext, initialNodes, initialEdges } from "../../contexts/canvas-context";
 
 export interface DbDesignerProps {
     example: string;
 }
 
 export function DbDesigner({ example }: DbDesignerProps) {
+    const location = useLocation();
+    const { id: projectId } = useParams();
+    const { setNodes, setEdges, setProjectName } = useCanvasContext();
+    const { projectName } = useCanvasContext();
+
+    useEffect(() => {
+        const projectData = location.state?.projectData;
+        if (projectData && projectData.data && projectData.data.canvas) {
+            setProjectName(projectData.data.projectName || "Untitled Project");
+
+            // Convert tables to React Flow nodes
+            const nodes = (projectData.data.canvas.tables || []).map((table: any) => ({
+                id: table.id,
+                type: "tableNode",
+                data: { label: table.name, ...table.data },
+                position: table.position,
+            }));
+
+            // Convert relationships to React Flow edges
+            const edges = (projectData.data.canvas.relationships || []).map((rel: any) => ({
+                id: rel.id,
+                source: rel.source,
+                sourceHandle: rel.sourceHandle,
+                target: rel.target,
+                targetHandle: rel.targetHandle,
+                type: rel.type,
+            }));
+
+            setNodes(nodes);
+            setEdges(edges);
+        } else if (!projectId) {
+            // Reset canvas for new project to initial state
+            setProjectName("New Project");
+            setNodes(initialNodes);
+            setEdges(initialEdges);
+        }
+    }, [location.state, setNodes, setEdges, setProjectName, projectId]);
 
     return (
         <div className="db-designer">
-            <CanvasProvider>
-                <Topbar projectName="Test" />
-                <div className="canvas">
-                    {/* Wrapped canvas in reactflow provider which allows us to get coordinates of mouse on canvas */}
-                    <ReactFlowProvider>
-                        <Canvas />
-                    </ReactFlowProvider>
-                </div>  
-            </CanvasProvider>
+            <Topbar projectName={projectName} />
+            <div className="canvas">
+                <ReactFlowProvider>
+                    <Canvas />
+                </ReactFlowProvider>
+            </div>
         </div>
     );
 }
