76609c9caebf5c3ea367000d244b21271bca96bb
Ben Northridge <bn00299@surrey.ac.uk>
2025-12-12 00:46:21 +0000

    swapped over to playwright

--- schemalab-ui/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-    preset: 'ts-jest',
-    testEnviroment: 'jsdom',
-    moduleNameMapper: {
-        "^@/(.*)$": "<rootDir>/src/$1"
-    },
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-};
